3ed84d53c4e02fe583e4a022a02b692f0cfdffe8
barruetr <richard.barruet@cgi.com>
2016-07-06 15:49:24 +0200

    port change

--- server.js
+++ server.js
@@ -4,6 +4,18 @@ const fs = require('fs');
 
 var app = express();
 
+var self = this;
+
+self.ipaddress  = process.env.OPENSHIFT_NODEJS_IP ||  process.env.OPENSHIFT_INTERNAL_IP;
+self.port       = process.env.OPENSHIFT_INTERNAL_PORT || process.env.OPENSHIFT_NODEJS_PORT || 8091;
+
+if (typeof self.ipaddress === "undefined") {
+     //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
+     //  allows us to run/test the app locally.
+     console.warn('No OPENSHIFT_*_IP var, using 127.0.0.1');
+     self.ipaddress = "127.0.0.1";
+};
+
 app.set("twig options", {
     strict_variables: false
 });
@@ -13,4 +25,4 @@ app.get('/', function(req, res){
   });
 });
 
-app.listen(8080);
+app.listen(self.port,self.ipaddress);
