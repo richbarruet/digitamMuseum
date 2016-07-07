7392d25e9d84cd2e9b8628ac692aab86e2ca1b70
barruetr <richard.barruet@cgi.com>
2016-07-07 11:40:04 +0200

    "==="

--- server.js
+++ server.js
@@ -1,15 +1,13 @@
-var express = require('express');
-var Twig = require("twig");
-const fs = require('fs');
+var express     = require('express');
+var Twig        = require("twig");
+var fs          = require('fs');
 
-var app = express();
-
-var self = this;
+var app         = express();
 
 ipaddress  = process.env.OPENSHIFT_NODEJS_IP ||  process.env.OPENSHIFT_INTERNAL_IP;
 port       = process.env.OPENSHIFT_INTERNAL_PORT || process.env.OPENSHIFT_NODEJS_PORT || 8091;
 
-if (typeof ipaddress === "undefined") {
+if (typeof ipaddress == "undefined") {
      //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
      //  allows us to run/test the app locally.
      console.warn('No OPENSHIFT_*_IP var, using 127.0.0.1');
