bdae731b594192e271ca886de5f1c14ef06594e6
barruetr <richard.barruet@cgi.com>
2016-07-07 14:44:52 +0200

    correctif

--- server.js
+++ server.js
@@ -14,14 +14,34 @@ if (typeof ipaddress == "undefined") {
      ipaddress = "127.0.0.1";
 };
 
+// Add headers
+/*app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});*/
+
 app.set("twig options", {
     strict_variables: false
 });
-
+/*
 app.get('/', function(req, res){
     res.redirect("/android");
 });
-
+*/
 app.get('/:platform', function(req, res){
     var platform = req.params.platform;
     
