0bf4302ef66b33be9427288aabc89ee3de61509f
barruetr <richard.barruet@cgi.com>
2016-07-08 10:23:26 +0200

    update

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ if (typeof ipaddress == "undefined") {
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    res.setHeader('Access-Control-Allow-Origin', "*");
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
